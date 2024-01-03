2ec22d13a09609f12c8f74cd57444c7ddb19af0b
vernetya <vernetya@gmail.com>
2024-01-03 15:38:15 +0100

    flash

---
+++ nvim/lua/yaya/plugins/flash.lua
@@ -0,0 +1,19 @@
+return {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    -- opts = {},
+    keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    },
+    configure = function()
+        local flash = require("flash")
+        flash.setup(
+            {
+                modes = {
+                    char = { enable = false }
+                }
+            }
+        )
+    end
+}
